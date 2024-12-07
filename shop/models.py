7f52b3ea2ad06c1fcc6beae17fc4051bcf941652
Marlon4789 <cuartasm844@gmail.com>
2024-12-06 21:07:14 -0500

    creando modelos

---
+++ shop/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=300, unique=True)
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])],
+
+        verbose_name = 'category' 
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    promotional_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    on_sale = models.BooleanField(default=False)
+    weight = models.FloatField()
+
+    GRINDING_TYPE_CHOICES = [
+        ('fine', 'fine'),
+        ('medium', 'medium'),
+        ('coarse', 'coarse')
+    ]
+    grinding_type = models.CharField(max_length=10, choices=GRINDING_TYPE_CHOICES)
+
+    availability = models.BooleanField(default=True)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='products/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+  
+    Category = models.ForeignKey(Category, related_name='products', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created_date']),
+        ]
+
+    def __str__(self):
+        return self.name
